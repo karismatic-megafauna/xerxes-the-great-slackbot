f19b05f05bdfe3523d05e7ead0e6744a12f457df
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-06-29 21:17:42 -0700

    Initial commit! Go Xerxes go!

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var Xerxes= require("./hellobot.js");
+var bodyParser = require("body-parser");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', function (req, res) { res.status(200).send('Hello World'); });
+
+app.use(function (err, req, res, next){
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+app.listen(port, function (){
+  console.log('Slackbot is listening on ' + port);
+});
+
+app.post('/hello', Xerxes);
